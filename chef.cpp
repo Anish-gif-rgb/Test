4a39de54263dd193fdb72fc831411004aecccdcb
Anish-gif-rgb <84190066+Anish-gif-rgb@users.noreply.github.com>
2022-05-30 17:59:13 +0530

    Add files via upload

---
+++ chef.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int A,B,x,y,Total;
+    cout<<"Chef went to a shop"<<endl;
+    cout<<"Number of pencils he bought ";
+    cin>>A;
+    cout<<"Number of pens he bought ";
+    cin>>B;
+    cout<<"cost of one pencil ";
+    cin>>x;
+     cout<<"cost of one pencil ";
+    cin>>y;
+    Total=A*x+B*y;
+    cout<<"total amount chef will spend to buy A pens and B pencils is"<<Total;
+    return 0;
+}
